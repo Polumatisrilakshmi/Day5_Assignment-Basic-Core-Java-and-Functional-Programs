94fcbe1664308b39545a30fa94d813e78ae06cbe
Polumatisrilakshmi <102572470+Polumatisrilakshmi@users.noreply.github.com>
2022-04-19 12:18:27 +0530

    Add files via upload

---
+++ FlipCoin.java
@@ -0,0 +1,19 @@
+public class FlipCoin {
+
+    public class FlipCoinProblem {
+
+
+        public static void main(String[] args) {
+            // flip coin
+            double num = Math.floor(Math.random());
+
+            if ( num < 0.5){
+                System.out.println("it is tails");
+            }else{
+                System.out.println("it is heads");
+            }
+
+
+        }
+    }
+}
